7ff7e9d8c8751e6dd377ca2553851f7cdf6bc365
docent28 <eaa67@mail.ru>
2021-10-20 17:08:18 +0300

    Сделали макет для погоды

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:ffi';
 
-void main(){
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
   runApp(const MyApp());
 }
 
@@ -9,6 +13,220 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.indigo,
+            ),
+            onPressed: () {},
+          ),
+          title: const Text(
+            "Weather",
+            style: TextStyle(color: Colors.black87),
+          ),
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.settings,
+                color: Colors.indigo,
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: _buildBody(),
+      ),
+    );
   }
+}
+
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Center(
+      child: Container(
+        width: 400,
+        child: Column(
+          children: [
+            _headerImage(),
+            SafeArea(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    _weatherDescription(),
+                    Divider(),
+                    _temperature(),
+                    Divider(),
+                    _temperatureForecast(),
+                    Divider(),
+                    _footerRatings(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Image _headerImage() {
+  return const Image(
+    image: NetworkImage(
+      "https://avatars.mds.yandex.net/get-zen_doc/169683/pub_5b37d5317aa92600aa96d74f_5b37d57aa9112400ae6e63b9/scale_1200",
+    ),
+    fit: BoxFit.cover,
+  );
+}
+
+Column _weatherDescription() {
+  return Column(
+    children: const [
+      Text(
+        "Tuesday - May 22",
+        style: TextStyle(
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      Divider(),
+      Text(
+        "In the south of the country it’s very windy and chilly. "
+        "There is a chance of some rain. The temperature is around 10º. "
+        "In the north and middle of the country we’re expecting a "
+        "thunderstorm. The west has the best weather today: "
+        "It’s sunny, windless and warm. The temperature is around 17º.",
+        style: TextStyle(
+          fontSize: 14,
+          color: Colors.black54,
+        ),
+      )
+    ],
+  );
+}
+
+Row _temperature() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(
+            Icons.wb_sunny,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+      SizedBox(width: 16),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                "15" + "\u2103" + " Clear",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.deepPurple,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                "Murmanskaya oblast, Murmansk",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+Wrap _temperatureForecast() {
+  return Wrap(
+    spacing: 10,
+    children: List.generate(
+      8,
+      (int index) {
+        return Chip(
+          label: Text(
+            "${index + 20}" + "\u2103",
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          avatar: Icon(
+            Icons.wb_cloudy,
+            color: Colors.blue.shade300,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+            side: BorderSide(color: Colors.grey),
+          ),
+          backgroundColor: Colors.grey.shade100,
+        );
+      },
+    ),
+  );
+}
+
+Row _footerRatings() {
+  var stars = Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      Icon(
+        Icons.star,
+        size: 15,
+        color: Colors.yellow[600],
+      ),
+      Icon(
+        Icons.star,
+        size: 15,
+        color: Colors.yellow[600],
+      ),
+      Icon(
+        Icons.star,
+        size: 15,
+        color: Colors.yellow[600],
+      ),
+      Icon(
+        Icons.star,
+        size: 15,
+        color: Colors.black,
+      ),
+      Icon(
+        Icons.star,
+        size: 15,
+        color: Colors.black,
+      ),
+    ],
+  );
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Text(
+        "Info with openweathermap.org",
+        style: TextStyle(
+          fontSize: 15,
+        ),
+      ),
+      stars,
+    ],
+  );
 }
