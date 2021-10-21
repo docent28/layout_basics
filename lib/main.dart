0091844d99f48ffe3e1e6cf2b73cd3244390700e
docent28 <eaa67@mail.ru>
2021-10-21 13:46:28 +0300

    оформили правильно через классы

--- lib/main.dart
+++ lib/main.dart
@@ -44,148 +44,173 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Widget _buildBody() {
-  return SingleChildScrollView(
-    child: Center(
-      child: Container(
-        width: 400,
-        child: Column(
-          children: [
-            _headerImage(),
-            SafeArea(
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    _weatherDescription(),
-                    Divider(),
-                    _temperature(),
-                    Divider(),
-                    _temperatureForecast(),
-                    Divider(),
-                    _footerRatings(),
-                  ],
+class _buildBody extends StatelessWidget {
+  const _buildBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Center(
+        child: Container(
+          width: 400,
+          child: Column(
+            children: [
+              _headerImage(),
+              SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      _weatherDescription(),
+                      Divider(),
+                      _temperature(),
+                      Divider(),
+                      _temperatureForecast(),
+                      Divider(),
+                      _footerRatings(),
+                    ],
+                  ),
                 ),
               ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _headerImage extends StatelessWidget {
+  const _headerImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image(
+      image: NetworkImage(
+        "https://avatars.mds.yandex.net/get-zen_doc/169683/"
+        "pub_5b37d5317aa92600aa96d74f_5b37d57aa9112400ae6e63b9/scale_1200",
+      ),
+      fit: BoxFit.cover,
+    );
+  }
+}
+
+class _weatherDescription extends StatelessWidget {
+  const _weatherDescription({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Text(
+          "Tuesday - May 22",
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Divider(),
+        Text(
+          "In the south of the country it’s very windy and chilly. "
+          "There is a chance of some rain. The temperature is around 10º. "
+          "In the north and middle of the country we’re expecting a "
+          "thunderstorm. The west has the best weather today: "
+          "It’s sunny, windless and warm. The temperature is around 17º.",
+          style: TextStyle(
+            fontSize: 14,
+            color: Colors.black54,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _temperature extends StatelessWidget {
+  const _temperature({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(
+              Icons.wb_sunny,
+              color: Colors.yellow,
             ),
           ],
         ),
-      ),
-    ),
-  );
-}
-
-Image _headerImage() {
-  return const Image(
-    image: NetworkImage(
-      "https://avatars.mds.yandex.net/get-zen_doc/169683/"
-          "pub_5b37d5317aa92600aa96d74f_5b37d57aa9112400ae6e63b9/scale_1200",
-    ),
-    fit: BoxFit.cover,
-  );
-}
-
-Column _weatherDescription() {
-  return Column(
-    children: const [
-      Text(
-        "Tuesday - May 22",
-        style: TextStyle(
-          fontSize: 32,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      Divider(),
-      Text(
-        "In the south of the country it’s very windy and chilly. "
-        "There is a chance of some rain. The temperature is around 10º. "
-        "In the north and middle of the country we’re expecting a "
-        "thunderstorm. The west has the best weather today: "
-        "It’s sunny, windless and warm. The temperature is around 17º.",
-        style: TextStyle(
-          fontSize: 14,
-          color: Colors.black54,
-        ),
-      )
-    ],
-  );
-}
-
-Row _temperature() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Icon(
-            Icons.wb_sunny,
-            color: Colors.yellow,
-          ),
-        ],
-      ),
-      SizedBox(width: 16),
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Text(
-                "15" + "\u2103" + " Clear",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.deepPurple,
+        SizedBox(width: 16),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  "15" + "\u2103" + " Clear",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.deepPurple,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                "Murmanskaya oblast, Murmansk",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    ],
-  );
-}
-
-Wrap _temperatureForecast() {
-  return Wrap(
-    spacing: 10,
-    children: List.generate(
-      8,
-      (int index) {
-        return Chip(
-          label: Text(
-            "${index + 20}" + "\u2103",
-            style: TextStyle(
-              fontSize: 15,
+              ],
             ),
-          ),
-          avatar: Icon(
-            Icons.wb_cloudy,
-            color: Colors.blue.shade300,
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(4),
-            side: BorderSide(color: Colors.grey),
-          ),
-          backgroundColor: Colors.grey.shade100,
-        );
-      },
-    ),
-  );
+            Row(
+              children: [
+                Text(
+                  "Murmanskaya oblast, Murmansk",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 }
 
-Row _footerRatings() {
+class _temperatureForecast extends StatelessWidget {
+  const _temperatureForecast({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 10,
+      children: List.generate(
+        8,
+        (int index) {
+          return Chip(
+            label: Text(
+              "${index + 20}" + "\u2103",
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            avatar: Icon(
+              Icons.wb_cloudy,
+              color: Colors.blue.shade300,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4),
+              side: BorderSide(color: Colors.grey),
+            ),
+            backgroundColor: Colors.grey.shade100,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _footerRatings extends StatelessWidget {
   var stars = Row(
     mainAxisSize: MainAxisSize.min,
     children: [
@@ -216,16 +241,20 @@ Row _footerRatings() {
       ),
     ],
   );
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      Text(
-        "Info with openweathermap.org",
-        style: TextStyle(
-          fontSize: 15,
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          "Info with openweathermap.org",
+          style: TextStyle(
+            fontSize: 15,
+          ),
         ),
-      ),
-      stars,
-    ],
-  );
+        stars,
+      ],
+    );
+  }
 }
